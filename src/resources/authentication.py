80ac8f2a45a19f560c00196e6696c4a3ef1f8b13
andrawicaksono <andrawicaksono14@gmail.com>
2022-10-24 17:18:43 +0700

    create sign in

--- src/resources/authentication.py
+++ src/resources/authentication.py
@@ -17,7 +17,28 @@ class SigninResource(Resource):
 
     def post(self, email, password):
         """ Signin """
-        return response({"data": "Signin"})
+
+        user = UserRepository.get_by_email(email)
+
+        if user is None:
+            return response({"error": "Email is not registered"}, 401)
+        
+        if not user.check_password(password):
+            return response({"error": "Password incorrect"}, 401)
+        
+        token = create_token(str(user.id))
+        user_information = {
+            "name" : user.name,
+            "email": user.email,
+            "phone_number": user.phone_number,
+            "type": user.type
+        }
+
+        return response({
+            "user_information": user_information,
+            "token": token,
+            "message": "Login success"
+        }, 200)
     
 
 
