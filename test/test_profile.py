dc9d0ed9e41deffee0bc3b3877b340f4eed24a24
andrawicaksono <andrawicaksono14@gmail.com>
2022-11-18 16:24:35 +0700

    create get shipping address test

--- test/test_profile.py
+++ test/test_profile.py
@@ -136,3 +136,74 @@ class TestShippingAddress(unittest.TestCase):
                 "data": address_data
             }
         )
+
+    def test_get(self):
+
+        user_data = {
+            "name": "test",
+            "email": "test@email.com",
+            "phone_number": "081234567890",
+            "password": "password",
+            "type": "buyer"
+        }
+
+        address_data = {
+            "name": "address name",
+            "phone_number": "087654321098",
+            "address": "address detail",
+            "city": "Birnin Zana"
+        }
+
+        UserRepository.create(user_data["name"], user_data["email"], user_data["phone_number"], user_data["password"], user_data["type"])
+
+        login_user = self.client.post(
+            "/sign-in",
+            data = json.dumps({
+                "email": user_data["email"],
+                "password": user_data["password"]
+            }),
+            content_type = "application/json"
+        )
+
+        self.token = json.loads(login_user.data.decode("utf-8"))["token"]
+
+        user_id = UserRepository.get_by_email(user_data["email"]).id
+
+        data = {
+            "id": str(user_id),
+            "name":address_data["name"],
+            "phone_number": address_data["phone_number"],
+            "address": address_data["address"],
+            "city": address_data["city"]
+        }
+
+        self.client.post(
+            "/user/shipping_address",
+            headers = {
+                "Authentication": self.token
+            },
+            data = json.dumps({
+                "name": address_data["name"],
+                "phone_number": address_data["phone_number"],
+                "address": address_data["address"],
+                "city": address_data["city"]
+            }),
+            content_type = "application/json"
+        )
+
+        response = self.client.get(
+            "/user/shipping_address",
+            headers = {
+                "Authentication": self.token
+            }
+        )
+
+        response_json = json.loads(response.data.decode("utf-8"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response_json,
+            {
+                "data": data
+            }
+        )
