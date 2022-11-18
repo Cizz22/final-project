4cffda8d54b2c127ce17c449b221a82a6a47ea2c
andrawicaksono <andrawicaksono14@gmail.com>
2022-11-18 15:44:37 +0700

    create post shipping address test

--- test/test_profile.py
+++ test/test_profile.py
@@ -52,7 +52,7 @@ class TestUser(unittest.TestCase):
 
         response = self.client.get(
             "/user",
-            headers={
+            headers = {
                 "Authentication": self.token
             }
         )
@@ -64,5 +64,75 @@ class TestUser(unittest.TestCase):
             response_json,
             {
                 "data": data
+            }
+        )
+
+class TestShippingAddress(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        server.config['SERVER_NAME'] = 'localhost:5053'
+        cls.client = server.test_client()
+
+    def setUp(self):
+        self.app_context = server.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_post(self):
+
+        user_data = {
+            "name": "test",
+            "email": "test@email.com",
+            "phone_number": "081234567890",
+            "password": "password",
+            "type": "buyer"
+        }
+
+        address_data = {
+            "name": "address name",
+            "phone_number": "087654321098",
+            "address": "address detail",
+            "city": "Birnin Zana"
+        }
+
+        UserRepository.create(user_data["name"], user_data["email"], user_data["phone_number"], user_data["password"], user_data["type"])
+
+        login_user = self.client.post(
+            "/sign-in",
+            data = json.dumps({
+                "email": user_data["email"],
+                "password": user_data["password"]
+            }),
+            content_type = "application/json"
+        )
+
+        self.token = json.loads(login_user.data.decode("utf-8"))["token"]
+
+        response = self.client.post(
+            "/user/shipping_address",
+            headers = {
+                "Authentication": self.token
+            },
+            data = json.dumps({
+                "name": address_data["name"],
+                "phone_number": address_data["phone_number"],
+                "address": address_data["address"],
+                "city": address_data["city"]
+            }),
+            content_type = "application/json"
+        )
+
+        response_json = json.loads(response.data.decode("utf-8"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response_json,
+            {
+                "data": address_data
             }
         )
