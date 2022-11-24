635fb55ec6e5e8c3251867afabb0263485116612
Cizz22 <cisatraa@gmail.com>
2022-11-24 13:41:19 +0700

    fix minor

--- src/routes/orders.py
+++ src/routes/orders.py
@@ -1,16 +1,16 @@
 """Define blueprint for orders"""
 
-from ast import Or
 from flask_restful import Api
 from flask import Blueprint
 
 from resources import OrdersResource, OrderResource
 
 ORDERS_BLUEPRINT = Blueprint("orders", __name__)
-Api(ORDERS_BLUEPRINT).add_resource(
-    OrdersResource, "/orders"
-)
 
 Api(ORDERS_BLUEPRINT).add_resource(
     OrderResource, "/order"
 )
+Api(ORDERS_BLUEPRINT).add_resource(
+    OrdersResource, "/orders"
+)
+
