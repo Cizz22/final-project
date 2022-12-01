929a23530f67e4c21e427076e5dc205e3f04e5bd
Cizz22 <cisatraa@gmail.com>
2022-12-01 21:55:21 +0700

    add product seeder

--- src/seeds/product.py
+++ src/seeds/product.py
@@ -16,5 +16,6 @@ def productSeeder():
     for c in category, 1:
         category = CategoryRepository.get_by(category_name=c).first()
         for i in range(5):
-            ProductRepository.create(title=f"{c}_{i}" , price=10000 , category_id=category.id,
-                                     condition="New", product_detail="This is Product detail")
+            product = ProductRepository.create(title=f"{c}_{i}" , price=10000 , category_id=category.id,
+                                               condition="New", product_detail=f"This is {c} detail")
+            ProductRepository.create_image(["image/dummy.jpg"], product_id=product.id)
