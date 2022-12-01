38bcb3e9554dbd8b8002fcba9590de74753b1027
Cizz22 <cisatraa@gmail.com>
2022-12-01 22:31:27 +0700

    fix categories

--- src/resources/home.py
+++ src/resources/home.py
@@ -27,7 +27,7 @@ class HomeCategoryResource(Resource):
 
     def get(self):
         """ Get all categories """
-        categories = CategoryRepository.get_by().paginate(1, 10, False)
+        categories = CategoryRepository.get_by().paginate(page=1, per_page=4)
         products = ProductRepository.get_by()
 
         res = {"data": [{
