151ee76dda9de31dd81ce394368de37190ee9438
Aulia Cisatra <63190964+Cizz22@users.noreply.github.com>
2024-08-08 18:27:33 +0700

    Update server.mk

--- makefiles/server.mk
+++ makefiles/server.mk
@@ -2,25 +2,25 @@
 # ¯¯¯¯¯¯¯¯¯¯¯
 
 server.install: ## Install server with its dependencies
-	docker-compose run --rm server pip install -r requirements.txt --user --upgrade --no-warn-script-location
+	docker compose run --rm server pip install -r requirements.txt --user --upgrade --no-warn-script-location
 
 server.start: ## Start server in its docker container
-	docker-compose up server
+	docker compose up server
 
 server.bash: ## Connect to server to lauch commands
-	docker-compose exec server bash
+	docker compose exec server bash
 
 server.daemon: ## Start daemon server in its docker container
-	docker-compose up -d server
+	docker compose up -d server
 
 server.stop: ## Start server in its docker container
-	docker-compose stop
+	docker compose stop
 
 server.storage: ## make storage file
-	docker-compose run server bash -c "mkdir -p static/image"
+	docker compose run server bash -c "mkdir -p static/image"
 
 server.nginx: ## make storage file
-	docker-compose up nginx -d
+	docker compose up nginx -d
 
 server.logs: ## Display server logs
 	tail -f server.log
