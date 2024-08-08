532a80e8920a113005fc83d23631dce34981d184
Aulia Cisatra <63190964+Cizz22@users.noreply.github.com>
2024-08-08 18:28:17 +0700

    Update format.mk

--- makefiles/format.mk
+++ makefiles/format.mk
@@ -2,7 +2,7 @@
 # ¯¯¯¯¯¯¯¯
 
 format.black: ## Run black on every file
-	docker-compose run --rm server bash -c "python vendor/bin/black src/ test/ --exclude vendor/"
+	docker compose run --rm server bash -c "python vendor/bin/black src/ test/ --exclude vendor/"
 
 format.isort: ## Sort imports
-	docker-compose run --rm server bash -c "python vendor/bin/isort -rc src/ test/ --skip vendor/ --skip src/models/__init__.py"
+	docker compose run --rm server bash -c "python vendor/bin/isort -rc src/ test/ --skip vendor/ --skip src/models/__init__.py"
