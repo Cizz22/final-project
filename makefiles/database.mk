b31e22621acc06ca7533e5970c58814a694d7818
Aulia Cisatra <63190964+Cizz22@users.noreply.github.com>
2024-08-08 18:27:56 +0700

    Update database.mk

--- makefiles/database.mk
+++ makefiles/database.mk
@@ -3,19 +3,19 @@
 
 
 database.connect: ## Connect to database
-	docker-compose exec db psql -U user -d db
+	docker compose exec db psql -U user -d db
 
 database.migrate: ## Create alembic migration file
-	docker-compose run --rm server python -m flask db migrate
+	docker compose run --rm server python -m flask db migrate
 
 database.upgrade: ## Upgrade to latest migration
-	docker-compose run --rm server python -m flask db upgrade
+	docker compose run --rm server python -m flask db upgrade
 
 database.downgrade: ## Downgrade latest migration
-	docker-compose run --rm server python -m flask db downgrade
+	docker compose run --rm server python -m flask db downgrade
 
 database.seeder: ## Run database seeder
-	docker-compose run --rm server python -m flask seeder 
+	docker compose run --rm server python -m flask seeder 
 
 database.drop: ## Drop database
-	docker-compose run --rm server python -m flask drop
+	docker compose run --rm server python -m flask drop
